c78d8f08de606a600815019b74f46de59bd60c37
Kleber Almeida Toyota <klebertiko@gmail.com>
2017-07-13 17:21:04 -0300

    Arrumando dependencias

--- src/main/java/br/com/concrete/mock/ApiApplication.java
+++ src/main/java/br/com/concrete/mock/ApiApplication.java
@@ -1,11 +1,9 @@
 package br.com.concrete.mock;
 
-import org.apache.http.impl.client.HttpClients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
