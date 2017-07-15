c034192d104fd1d5f4e80b833be2280a2ae05e9d
André Formento <andreformento.sc@gmail.com>
2017-07-14 21:53:32 -0300

    Corrige erro de compilação

--- src/main/java/br/com/concrete/mock/ApiApplication.java
+++ src/main/java/br/com/concrete/mock/ApiApplication.java
@@ -2,6 +2,7 @@ package br.com.concrete.mock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
